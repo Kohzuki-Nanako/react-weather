580a2e7b4baa107700cfe37fe810a4755a626941
上月那奈子 <chna18018@g.nihon-u.ac.jp>
2020-06-27 01:00:33 +0900

    upload3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Image from 'react-image-resizer';
 import React from "react";
+import axios from "axios";
+import Weather from "./components/Weather/Weather";
+import Form from "./components/Form/Form";
 import tenkikun from "./media/kappa_man2.png"
 import irasutoya from "./media/スクリーンショット 2020-06-25 0.36.56.png"
 import rainbow from "./media/sky_line04_rainbow.png"
-import Project from "./project";
+
 
 const style = {
     image: {
@@ -12,7 +15,7 @@ const style = {
     },
 };
 
-
+const api_key = "4137c4ce4404f78ffb0df598b3efd8ea"
 
 const App = () => {
   return <div>
@@ -63,7 +66,7 @@ const App = () => {
         <img src={rainbow} alt ="虹"/>
       </figure>
       <p className="title">世界の天気は？</p>
-      <Project />
+      <Forcast />
     </div>
    </div>
   </div>
@@ -75,6 +78,56 @@ const App = () => {
  </div>
 </div>
 };
+
+
+const Forcast = () => {
+
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  // const [icon, setIcon] = useState();
+  const [description, setDescription] = useState("");
+  const [temp_min, setTemp_min] = useState("");
+  const [temp_max, setTemp_max] = useState("");
+  const [temp, setTemp] = useState("");
+  // const [error, setError] = useState(false);
+
+
+  const getWeather = async (e) => {
+      e.preventDefault();
+
+      const city = e.target.elements.city.value;
+      const country = e.target.elements.country.value;
+      // console.log("got here")
+      if (city && country) {
+          const api_call = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+          const response = await api_call;
+          // console.log(response);
+          // return response;
+          setCity(response.data.name)
+          setCountry(response.data.sys.country);
+          setTemp(response.data.main.temp);
+          setTemp_max(response.data.main.temp_max);
+          setTemp_min(response.data.main.temp_min);
+          setDescription(response.data.weather[0].description);
+          console.log(response);
+
+      }
+  }
+  return (
+      <div className="App">
+          <Weather 
+              city = {city} 
+              country = {country} 
+              temp = {temp} 
+              temp_max = {temp_max}
+              temp_min = {temp_min}
+              description = {description}
+          />
+          <Form loadWeather={getWeather} />
+      </div>
+  )
+}
+
 
 
 export default App;
